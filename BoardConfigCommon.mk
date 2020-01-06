232a31096561e8c2a13605c5f6bc72ce29568049
zm95 <840801426@qq.com>
2020-01-07 00:42:03 +0800

    sdm660-common:Lineage-ify

--- BoardConfigCommon.mk
+++ BoardConfigCommon.mk
@@ -247,4 +247,4 @@ WPA_SUPPLICANT_VERSION := VER_0_8_X
 WIFI_HIDL_FEATURE_DUAL_INTERFACE := true
 
 # inherit from the proprietary version
--include vendor/smartisan/sdm660-common/BoardConfigVendor.mk
+include vendor/smartisan/sdm660-common/BoardConfigVendor.mk
